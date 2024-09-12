47241b2570f2c9bfa6af28718ffc62f50605ee44
Ajmaldevala <ajmaldevala@gmail.com>
2024-09-12 10:58:29 +0530

    update app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-// import { UsersController } from './users/users.controller';
-// import { UsersService } from './users/users.service';
 
 @Module({
   imports: [],
-  // controllers: [AppController, UsersController],
-  // providers: [AppService, UsersService],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
